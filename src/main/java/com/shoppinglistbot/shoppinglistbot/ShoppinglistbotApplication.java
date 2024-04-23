895891c4671fa8b0543557382efbf4297d3a26f8
renan <renan.stos077@gmail.com>
2024-04-23 13:56:44 -0300

    initial commit

---
+++ src/main/java/com/shoppinglistbot/shoppinglistbot/ShoppinglistbotApplication.java
@@ -0,0 +1,13 @@
+package com.shoppinglistbot.shoppinglistbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppinglistbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppinglistbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoppinglistbot/shoppinglistbot/ShoppinglistbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppinglistbot.shoppinglistbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppinglistbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
