0f541bbc1f81c70f125287376f6d461cad6d2997
renan <renan.stos077@gmail.com>
2024-04-26 15:35:01 -0300

    SlashCommandListener class creation

---
+++ src/main/java/com/shoppinglistbot/shoppinglistbot/listeners/SlashCommandListener.java
@@ -0,0 +1,30 @@
+package com.shoppinglistbot.shoppinglistbot.listeners;
+
+import com.shoppinglistbot.shoppinglistbot.commands.SlashCommand;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Collection;
+import java.util.List;
+
+@Component
+public class SlashCommandListener {
+
+    private final List<SlashCommand> commands;
+
+    public SlashCommandListener(List<SlashCommand> slashCommands, GatewayDiscordClient client) {
+        commands = slashCommands;
+
+        client.on(ChatInputInteractionEvent.class, this::handle);
+    }
+
+    public Mono<Void> handle(ChatInputInteractionEvent event) {
+        return Flux.fromIterable(commands)
+                .filter(command -> command.getName().equals(event.getCommandName()))
+                .next()
+                .flatMap(command -> command.handle(event));
+    }
+}
