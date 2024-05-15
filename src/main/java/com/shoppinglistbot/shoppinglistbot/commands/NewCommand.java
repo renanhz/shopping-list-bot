a63b98f5d09df5aca5850e20ca18993531fefd53
renan <renan.stos077@gmail.com>
2024-05-15 11:20:37 -0300

    NewCommand post call

--- src/main/java/com/shoppinglistbot/shoppinglistbot/commands/NewCommand.java
+++ src/main/java/com/shoppinglistbot/shoppinglistbot/commands/NewCommand.java
@@ -1,8 +1,15 @@
 package com.shoppinglistbot.shoppinglistbot.commands;
 
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 import reactor.core.publisher.Mono;
 
+@Component
 public class NewCommand implements SlashCommand{
 
     @Override
@@ -12,6 +19,27 @@ public class NewCommand implements SlashCommand{
 
     @Override
     public Mono<Void> handle(ChatInputInteractionEvent e) {
-        return null;
+        Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+        Long channelId = e.getInteraction().getChannelId().asLong();
+
+        RestClient httpClient = RestClient.create();
+
+        ResponseEntity<String> result = httpClient.post()
+                .uri("http://localhost:8081/api/v1/shopping-list/{channelId}", channelId)
+                .retrieve()
+                .toEntity(String.class);
+
+
+        String replyContent = "";
+        if (result.getStatusCode().equals(HttpStatus.CREATED)) {
+            replyContent = "Lista criada!";
+        } else {
+            replyContent = "Falha ao criar lista";
+            LOGGER.error(result.getStatusCode().toString());
+        }
+
+        return e.deferReply().then(e.editReply(replyContent)).then();
     }
+
 }
