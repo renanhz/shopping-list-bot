422e37a126d1f73ddd4fd607d8913e85fda773f2
renan <renan.stos077@gmail.com>
2024-05-20 11:45:26 -0300

    post request additem

--- src/main/java/com/shoppinglistbot/shoppinglistbot/commands/AddCommand.java
+++ src/main/java/com/shoppinglistbot/shoppinglistbot/commands/AddCommand.java
@@ -1,12 +1,18 @@
 package com.shoppinglistbot.shoppinglistbot.commands;
 
+import com.shoppinglistbot.shoppinglistbot.model.Item;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import discord4j.core.object.command.ApplicationCommandInteractionOption;
 import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+import org.springframework.web.client.RestClient;
 
 import java.util.Optional;
 
@@ -27,15 +33,28 @@ public class AddCommand implements SlashCommand{
                 .map(ApplicationCommandInteractionOptionValue::asString)
                 .get();
 
-        StringBuilder reply = new StringBuilder();
-        reply.append("Item adicionado: ").append(itemName).append(" Quantidade: ");
-
-        Optional<Long> itemQuantity = e.getOption("qtd")
+        String itemQuantity = e.getOption("qtd")
                 .flatMap(ApplicationCommandInteractionOption::getValue)
-                .map(ApplicationCommandInteractionOptionValue::asLong);
+                .map(ApplicationCommandInteractionOptionValue::asString)
+                .orElse("1");
 
-        itemQuantity.ifPresentOrElse(reply::append, () -> reply.append(1));
+        Item item = new Item(itemName, itemQuantity);
 
-        return e.reply().withContent(reply.toString());
+        Long channelId = e.getInteraction().getChannelId().asLong();
+
+        String reply = RestClient.create().post()
+                .uri("http://localhost:8081/api/v1/item/{channelId}", channelId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(item)
+                .exchange((request, response) -> {
+                    if (response.getStatusCode().is4xxClientError()) {
+                        return "É preciso criar uma lista antes de adicionar um item, use o comando /new";
+                    } else {
+                        return "Item adicionado: " + itemName + ", Quantidade: " + itemQuantity;
+                    }
+                });
+
+
+        return e.deferReply().then(e.editReply(reply).then());
     }
 }
