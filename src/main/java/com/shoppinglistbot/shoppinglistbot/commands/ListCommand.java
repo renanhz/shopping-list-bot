beda49157146ccde108adb6b26becbe96909117d
renan <renan.stos077@gmail.com>
2024-05-21 14:16:52 -0300

    Embed reply listcommand

--- src/main/java/com/shoppinglistbot/shoppinglistbot/commands/ListCommand.java
+++ src/main/java/com/shoppinglistbot/shoppinglistbot/commands/ListCommand.java
@@ -1,8 +1,17 @@
 package com.shoppinglistbot.shoppinglistbot.commands;
 
+import com.shoppinglistbot.shoppinglistbot.model.Item;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.spec.EmbedCreateFields;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.rest.util.Color;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class ListCommand implements SlashCommand{
@@ -13,6 +22,53 @@ public class ListCommand implements SlashCommand{
 
     @Override
     public Mono<Void> handle(ChatInputInteractionEvent e) {
-        return null;
+        RestClient httpClient = RestClient.create();
+        Long channelId = e.getInteraction().getChannelId().asLong();
+
+        Item[] itemArray = httpClient.get().
+                uri("http://localhost:8081/api/v1/shopping-list/{channelId}", channelId)
+                .retrieve()
+                .toEntity(Item[].class)
+                .getBody();
+
+        List<Item> items = Arrays.asList(itemArray);
+
+        EmbedCreateSpec embed = EmbedCreateSpec.builder()
+                .color(Color.YELLOW)
+                .title("Lista de compras")
+                .build();
+
+        if (items.isEmpty()) {
+            embed = embed.withDescription("A lista está vazia");
+        } else {
+            embed = embed.withDescription("Items da Lista: ");
+
+            List<EmbedCreateFields.Field> fields = new ArrayList<>();
+            for (Item item:items) {
+                fields.add(createField(item));
+            }
+            embed = embed.withFields(fields);
+        }
+
+        return e.deferReply().then(e.editReply().withEmbeds(embed).then());
+    }
+
+    public EmbedCreateFields.Field createField(Item item) {
+        return new EmbedCreateFields.Field() {
+            @Override
+            public String name() {
+                return item.name();
+            }
+
+            @Override
+            public String value() {
+                return item.quantity();
+            }
+
+            @Override
+            public boolean inline() {
+                return false;
+            }
+        };
     }
 }
