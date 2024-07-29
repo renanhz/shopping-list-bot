6c8de6db586a9e16ecc4eae20cb48a6bb58f9c27
renan <renan.stos077@gmail.com>
2024-07-29 10:27:02 -0300

    ShoppingListService createList

--- src/main/java/com/shoppinglistbot/shoppinglistbot/services/ShoppingListService.java
+++ src/main/java/com/shoppinglistbot/shoppinglistbot/services/ShoppingListService.java
@@ -1,7 +1,11 @@
 package com.shoppinglistbot.shoppinglistbot.services;
 
 import com.shoppinglistbot.shoppinglistbot.model.Item;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -14,9 +18,7 @@ public class ShoppingListService {
     private RestClient httpClient;
     private final String baseUrl = "http://localhost:8081/api/v1/";
 
-    public ShoppingListService() {
-        this.httpClient = RestClient.create();
-    }
+    public ShoppingListService() {this.httpClient = RestClient.create();}
 
     public List<Item> getItems(Long channelId) {
         String finalUrl = baseUrl + "shopping-list/{channelId}";
@@ -31,7 +33,7 @@ public class ShoppingListService {
     }
 
     public String addItem(Long channelId, Item item) {
-        String reply = RestClient.create().post()
+        String reply = httpClient.post()
                 .uri("http://localhost:8081/api/v1/item/{channelId}", channelId)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(item)
@@ -42,6 +44,25 @@ public class ShoppingListService {
                         return "Item adicionado: " + item.name() + ", Quantidade: " + item.quantity();
                     }
                 });
+
+        return reply;
+    }
+
+    public String createList(Long channelId) {
+        Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+        ResponseEntity<String> result = httpClient.post()
+                .uri("http://localhost:8081/api/v1/shopping-list/{channelId}", channelId)
+                .retrieve()
+                .toEntity(String.class);
+
+        String reply = "";
+        if (result.getStatusCode().equals(HttpStatus.CREATED)) {
+            reply = "Lista criada!";
+        } else {
+            reply = "Falha ao criar lista";
+            LOGGER.error(result.getStatusCode().toString());
+        }
 
         return reply;
     }
